e3e4bc0432b5b5fce516ab1ed316171c9ce1de99
dryigit <fatihyigit6@gmail.com>
2022-02-15 00:12:59 +0300

    parallel testing

--- com.CucumberBatch44/src/test/java/runners/ParalelTestRunner1.java
+++ com.CucumberBatch44/src/test/java/runners/ParalelTestRunner1.java
@@ -1,2 +1,17 @@
-package runners;public class ParalelTestRunner1 {
+package runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin={"html:target\\Pcucumber-reports1.html",
+				"json:target/json-reports/Pcucumber1.json",
+				"junit:target/xml-report/Pcucumber1.xml"},
+		features="src/test/resources/features",
+		glue="stepdefinitions",
+		tags="@paralel1",
+		dryRun=false
+)
+public class ParalelTestRunner1 {
 }
