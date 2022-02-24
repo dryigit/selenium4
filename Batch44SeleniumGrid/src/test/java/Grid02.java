b9f1fae19867471f091411de4f02c375f54a8a03
dryigit <fatihyigit6@gmail.com>
2022-02-24 21:50:48 +0300

    seleniumGrid

---
+++ Batch44SeleniumGrid/src/test/java/Grid02.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Grid02 {
+
+public static void main(String[] args) throws MalformedURLException {
+	
+	
+	WebDriver driver=new RemoteWebDriver (new URL("http://192.168.1.9:4444"), new FirefoxOptions());
+	
+	driver.get("https://google.com");
+	System.out.println(driver.getTitle());
+	System.out.println(driver.getCurrentUrl());
+	driver.close();
+	
+	
+}
+
+}
